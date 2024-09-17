5d3717e503af678d171220ab3cf16a6637218c9c
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-09-17 12:01:17 +0100

    feat: add Nutritions class, an entity class to map the nested nutritions object within the fruiyvice objects

---
+++ app/src/main/java/com/northcoders/fruityviceapp/model/Nutritions.java
@@ -0,0 +1,13 @@
+package com.northcoders.fruityviceapp.model;
+
+public class Nutritions {
+
+    private double calories;
+    private double fat;
+    private double sugar;
+    private double carbohydrates;
+    private double protein;
+
+
+
+}
